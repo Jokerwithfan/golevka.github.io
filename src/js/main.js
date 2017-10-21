f22472b0984669a1f3c98f5f69cf786875aa51af
Peach <scdzwyxst@gmail.com>
2017-10-22 00:43:16 +0800

    Add anchor feature for the navbar

--- src/js/main.js
+++ src/js/main.js
@@ -7,10 +7,28 @@ const opsObj = new OnePageScroll({
   section: 'section',
 });
 
-opsObj.afterScroll(() => {
-});
+function updateNavAnchor(i) {
+  const previous = document.querySelector('.primary-navbar .active');
+  const target = document.querySelector(`.primary-navbar a:nth-child(${i + 1})`);
+  const iconWidth = (target.querySelector('i') || { offsetWidth: 0 }).offsetWidth;
+  const anchor = document.querySelector('.primary-navbar .anchor');
+  const offsetLeft = (target.offsetLeft + ((target.offsetWidth - iconWidth) / 2) + iconWidth)
+    - (anchor.offsetWidth / 2);
 
-opsObj.beforeScroll(() => {
+  anchor.style.transform = `translateX(${offsetLeft}px)`;
+  anchor.style.webkitTransform = anchor.style.transform;
+  anchor.style.mozTransform = anchor.style.transform;
+  anchor.style.msTransform = anchor.style.transform;
+  anchor.style.oTransform = anchor.style.transform;
+
+  if (previous) {
+    previous.classList.remove('active');
+  }
+  target.classList.add('active');
+}
+
+opsObj.beforeScroll((i) => {
+  updateNavAnchor(i);
 });
 
 document.addEventListener('click', ({ target }) => {
@@ -20,6 +38,9 @@ document.addEventListener('click', ({ target }) => {
       break;
     case 'scroll-to-top':
       opsObj.scrollTo(0);
+      break;
+    case 'scroll-to-page':
+      opsObj.scrollTo(target.getAttribute('ops-index') * 1);
       break;
     default:
   }
