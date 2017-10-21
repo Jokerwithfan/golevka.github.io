87ea6343f3ed235f3d3cced8df4d41a3acf5ddb0
Peach <scdzwyxst@gmail.com>
2017-10-21 22:56:35 +0800

    Remove dependency purejs-onepage-scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,1 @@
-import onePageScroll from 'onepage-scroll/onepagescroll.min'; // eslint-disable-line import/no-extraneous-dependencies
-
 require('../scss/main.scss');
-
-onePageScroll('main', {
-  sectionContainer: 'section',
-  updateURL: true,
-  pagination: false,
-});
